868f25965bc45e7aa627adad2eefd8c9adbf360c
Jack Myers <jmyers@launchpointeps.com>
2022-08-12 16:02:24 -0700

    better comments in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,10 +18,20 @@ if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
   NP_INC=""
 fi
 
+# This is done on two lines so that the command will return failure info if it fails
 PKG_CONFIG=$(which pkg-config)
 export PKG_CONFIG
 
+
 cd "${SRC_DIR}"
+
+# MESON_ARGS is used within setup.py to pass extra arguments to meson
+# We need these so that dependencies on the build machine are not incorrectly used by meson when building for a different target
 export MESON_ARGS="-Dipopt_dir=${PREFIX} -Dincdir_numpy=${NP_INC} -Dpython_target=${PYTHON} ${EXTRA_FLAGS}"
+
+# We use this instead of pip install . because the way meson builds from within a conda-build process puts the build
+# artifacts where pip install . can't find them. Here we explicitly build the wheel into the working director, wherever that is
+# and then tell pip to install the wheel in the working directory. Also, python -m build is now the recommended way to build
+# see https://packaging.python.org/en/latest/tutorials/packaging-projects/
 python -m build -n -x .
 pip install --prefix "${PREFIX}" --no-deps --no-index --find-links dist pyoptsparse
